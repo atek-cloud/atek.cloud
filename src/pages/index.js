b1c0b0bda0208b27fdf575a915ec9433def849e8
Paul Frazee <pfrazee@gmail.com>
2021-09-06 14:11:41 -0500

    More front page improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,12 +32,14 @@ export default function Home() {
       <main>
         <div class={styles.notice}>
           <div>
+            <h3>Hi everybody!</h3>
             <p>
-              Hi everybody! <Link to="https://twitter.com/pfrazee">Paul Frazee</Link> here.
-              Atek is still a work in progress. I created this website to help get contributors onboarded.
+              <Link to="https://twitter.com/pfrazee">Paul Frazee</Link> here.
+              Atek is still a work in progress. I created this site to help get contributors onboarded.
             </p>
             <p>
-              Check out the <Link to="/blog/hello-world">introductory blogpost</Link> to learn more about what Atek is about.
+              Check out the <Link to="/docs/manual/dev/architecture">Architecture Document</Link> and
+              the <Link to="/blog/hello-world">Introductory Blogpost</Link> to learn more about what Atek is about.
               We have a <Link to="https://discord.com/channels/883086455092674602/883086456464224356">Discord channel</Link> and a <Link to="https://github.com/atek-cloud/atek/discussions">GitHub discussion board</Link> where you can get involved.
             </p>
           </div>
@@ -46,7 +48,7 @@ export default function Home() {
           <div>
             <h2>What is Atek?</h2>
             Atek is a personal cloud for small home servers like <Link to="https://www.raspberrypi.org/">Raspberry Pis</Link>.
-            It uses peer-to-peer tech to connect your devices and other home servers so you can share posts,
+            It uses peer-to-peer tech to connect your devices so you can share posts,
             photos, chats, and applications with the privacy and control you want.
           </div>
         </div>
